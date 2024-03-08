f73d803801b212112c352514b6432bc3eccd811d
Prasham1710 <jainprasham17@gmail.com>
2024-03-08 05:33:59 +0530

    product description i made by ids

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,42 @@
+import Image from "next/image";
 import ProductCard from "./components/ProductCard";
 import { prisma } from '@/app/lib/db/prisma';
+import Link from "next/link";
 export default async function Home() {
-   const products =  await prisma.product.findMany({
-    orderBy: { createdAt: "desc" },
+   const products = await prisma.product.findMany({
+    orderBy: { id: "desc" },
    });
   return (
-    <div>
-      <ProductCard product={products[0]}/>
-    </div>
+    <div className="flex flex-col items-center">
+     
+        <div className="hero rounded-xl bg-base-200">
+          <div className="hero-content flex-col lg:flex-row">
+            <Image
+              src={products[0].imageUrl}
+              alt={products[0].name}
+              width={400}
+              height={800}
+              className="w-full max-w-sm rounded-lg shadow-2xl"
+              priority
+            />
+            <div>
+              <h1 className="text-5xl font-bold">{products[0].name}</h1>
+              <p className="py-6">{products[0].description}</p>
+              <Link
+                href={"components/products/" + products[0].id}
+                className="btn-primary btn"
+              >
+                Check it out
+              </Link>
+            </div>
+          </div>
+        </div>
+      
+           <div className="my-4 grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+            {products.slice(1).map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+           </div>
+        </div>
   );
 }
